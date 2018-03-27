b3cc42f0f01545329250064fa35e75c7675b7204
andy tham <thamandyy@gmail.com>
2018-03-26 21:08:47 -0400

    connection established

--- index.js
+++ index.js
@@ -10,10 +10,36 @@ const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = proces
 Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
+// connect to db
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
+  operatorsAliases: false,
 
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: './db/database.db'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
-
+app.get('/', (req, res) => {
+  res.send('buggah')
+})
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
   res.status(500).send('Not Implemented');
